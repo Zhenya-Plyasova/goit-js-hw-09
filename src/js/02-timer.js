cc83687c2bf3e7ace06fbb834b69100f8bc85052
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-01-24 17:19:41 +0200

    2 tasks solved

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,25 +3,76 @@ import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.6.min.css";
 
+// let newDate = new Date();
+let newDate = Date.now();
+let amountOfMs = 0;
+let chosenDate = 0;
+let restOfTime = 0;
+
+const input = document.querySelector("#datetime-picker");
+const daysToFin = document.querySelector("[data-days]");
+const hoursToFin = document.querySelector("[data-hours]");
+const minutesToFin = document.querySelector("[data-minutes]");
+const secondsToFin = document.querySelector("[data-seconds]");
+const start = document.querySelector("button");
+start.setAttribute("disabled", true);
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        // if (selectedDates<defaultDate){
-        //     window.alert("Please choose a date in the future")
-        // }
-      console.log(selectedDates[0]);
+        if (selectedDates[0].getTime()<=newDate){
+            window.alert("Please choose a date in the future");
+                    }
+        else{
+            start.removeAttribute("disabled");
+        }
+
+    amountOfMs =selectedDates[0].getTime()-newDate;
+    chosenDate = selectedDates[0].getTime();
     }
   };
 
-const input = document.querySelector("#datetime-picker");
-const days = document.querySelector("[data-days]");
-const hours = document.querySelector("[data-hours]");
-const minutes = document.querySelector("[data-minutes]");
-const seconds = document.querySelector("[data-seconds]");
-const start = document.querySelector("button");
+flatpickr("#datetime-picker", options);
 
-flatpickr("#datetime-picker", {options});
+start.addEventListener("click", onStartClick);
 
+function onStartClick(){
+    const intervalID = setInterval(()=>{
+        const currentTime = Date.now();
+        restOfTime = chosenDate - currentTime;
+  
+        setTimer();
+        if (restOfTime<=1000){
+            clearInterval(intervalID);
+        }
+    }, 1000);
+
+}
+
+function pad(value){
+    return String(value).padStart(2, '0');
+}
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = pad(Math.floor(ms / day));
+    const hours = pad(Math.floor((ms % day) / hour));
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+  };
+
+  function setTimer(){
+    const { days, hours, minutes, seconds } = convertMs(restOfTime);
+    daysToFin.textContent = `${days}`;
+    hoursToFin.textContent = `${hours}`;
+    minutesToFin.textContent = `${minutes}`;
+    secondsToFin.textContent = `${seconds}`;
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// const delayInput = document.getElementsByName("delay")[0];
+// const stepInput = document.getElementsByName("step")[0];
+// const amountInput = document.getElementsByName("amount")[0];
+const button = document.querySelector("button");
+
+console.log("Hi");
+
+button.addEventListener("click", onButtonClick);
+function onButtonClick(){
+  console.log("Hi-2");
+// const delay = delayInput.value;
+// console.log(delay);
+// const step = stepInput.value;
+// const amount = amountInput.value;
+
+// console.log(`delay ${delay}, step ${step}, valu] ${amount}`);
+// for (let i = 1; i <=amount; i++) {
+//   createPromise(i, (delay+(i-1)*step))
+
+// }
+
+};
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   return new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//       resolve();
+//     }, delay);
+//   });
+//   if (shouldResolve) {
+//     console.log(`promis #${position}, with delay ${delay}`);
+//   } else {
+//     console.log('error');
+//   }
+// }
+
